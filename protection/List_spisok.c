d6be60214791755a50c88c85d1fcdff424bf7a61
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2019-06-27 16:27:44 +0300

    Create List_spisok.c

---
+++ protection/List_spisok.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAXSIZE 10000000
+
+typedef struct Node{   
+    int data;  
+    struct Node *prev;   
+}Node;
+
+
+typedef struct Stack{
+   struct Node *head;
+}Stack;
+
+
+
+void pop(Node** head) {
+  Node* a = *head;
+  *head = (*head)->prev;
+  free(a); 
+}
+
+void push(Node** head, int x)
+{
+
+   Node* NewNode = (Node*)malloc(sizeof(struct Node));
+   NewNode->data = x;
+   NewNode->prev = *head;
+   *head = NewNode;
+}
+
+
+// функция top
+int top(Node* S) {   
+    return S->data;  
+}
+
+
+int isEmpty(Node* head){
+    if (head == NULL) return 0;
+    return 1;
+}
+               
+                     
+int main(){
+    Node* head = NULL;
+    clock_t c1 = clock();
+    
+    for(int i = 0; i < MAXSIZE; i++){
+        push(&head, i);
+    }
+    int t = (int)((clock() - c1));
+    printf("time of push: %.3f sec \n", (float)t/CLOCKS_PER_SEC);
+    clock_t c2 = clock();
+    for(int i = 1; i < MAXSIZE; i++){
+        pop(&head);
+    }
+    t = (int)((clock() - c2));
+    printf("time of pop: %.3f sec", (float)t/CLOCKS_PER_SEC);
+    return 0;    
+}

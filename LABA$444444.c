91f8756ab5797b1501d4c10708c222898cb80569
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2019-09-08 11:03:19 +0300

    Update LABA$444444.c

--- LABA$444444.c
+++ LABA$444444.c
@@ -1,120 +1,136 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <iostream>
+#include <cstring>
+#include <fstream>
 
-#define N 500
-#define type char
+using namespace std;
 
-struct SArray {
-        type arr[N];
-        int topIndex;
+
+class Vector {
+ public:
+    int* data;
+    int size;
+// объявлением другом, если закрою массив    
+    friend ostream &operator<<(ostream &stream, Vector obj);
 };
 
-void pushArray(struct SArray *a, type value);
-type popArray(struct SArray *a);
-type topArray(struct SArray *a);
-struct SArray* initSArray();
-int isEmptyArray(struct SArray *a);
+
+// перегрузка оператора вывода 
+
+ostream &operator<<(ostream &stream, Vector obj){
+    int i = 0;
+    for (i; i < sizeof(obj)/sizeof(obj.data[0])-1 ; i++)
+    {
+        stream << obj.data[i] << ", ";
+    }
+    stream << obj.data[i] << "\n";
+    return stream;
+}
 
 
-struct S{
-        struct Elem *headElem;//голова списка
-	struct Elem *firstElem;
-};
-
-struct Elem{
-        char value;
-        struct Elem *next;
-};
-
-void push(struct S *a, char value);
-char pop(struct S *a);
-char top(struct S *a);
-struct S* initS();
-int isEmpty(struct S *a);
+void split(char *buf, Vector &obj){
+    char * tok = " ,";
+    char *pch = strtok(buf, tok);//подстрока до первого разделителя
+    for(int i = 0; pch && i < sizeof(obj.data)/sizeof(obj.data[0]); i++)
+    {
+        obj.data[i] = atoi(pch);
+        pch = strtok(0,tok);
+    }
+}
 
 
-void push(struct S *a, char value){//вставка в голову списка
-        struct Elem *newElem = malloc(sizeof(struct Elem));
-        newElem -> value = value;
-        if(isEmpty(a)){
-                newElem -> next = NULL;
-		a -> firstElem = newElem;
+void put_from_file(const char *fileName, Vector &obj){
+    char ch;
+    char *buf; //dont know
+    int count = 0;
+    int i,j = 0;
+    ifstream in(fileName, ios::in | ios::binary);
+    if (!in){
+        cout << "Не удается открыть файл.\n";
+        return ;
+    }
+    while (in.get(ch))
+       count++;
+    buf = new char[count];   
+    obj.data = new int[count];
+    in.seekg(ios::beg); // или fstream стр 468
+    
+    in.read(buf, count);
+    
+    split(buf, obj);
+    
+    while (in){
+        in.get(ch);
+        if(in && (ch <='9') && (ch >= '1')) buf[i++] = ch; // как сделать динамическую память
+        else 
+        {
+            buf[i] = ch;
+            obj.data[j++] = atoi(buf);    
         }
-        else newElem -> next = a -> headElem;
-        a -> headElem = newElem;
-        return;
+    }
+    
 }
 
-void pushArray(struct SArray *a, type value){
-	a -> topIndex++;
-	a -> arr[a->topIndex] = value;
-	return;
-}
 
-char pop(struct S *a){
-        if(!isEmpty(a)){
-            char Elem = top(a);
-	    struct Elem *tmp = a -> headElem;
-	    while(tmp->next->next){
-	    	tmp = tmp->next;
-	    }
-	    tmp -> next = NULL;
-            a -> firstElem = tmp;
-            return Elem;
+
+/*
+void put_data(Vector &vect, int* arr, int len){
+    vect.data = new int[len];
+    vect.size = len;
+    for (int i = 0; i < len; i++){
+        vect.data[i] = arr[i];
+    }
+  //  delete [] arr;
+}
+void print_vector(Vector &vect){
+    cout << "Вектор: (";
+    for (int i = 0; i < vect.size; i++){
+        if (i != 0) cout << ",";
+        cout << vect.data[i];
+    }
+    cout << ")";
+}
+*/
+
+
+void change_vector(Vector &vector, int i , int len){
+    if(len > 2){
+        if(len % 2 == 1){
+            change_vector(vector,i,len/2+1);
+            change_vector(vector,i + len/2 ,len/2);
+        }else{
+             change_vector(vector,i,len/2);
+             change_vector(vector,i + len/2,len/2);
         }
+    }
+
+    if(len == 2 && (vector.data[i+1] < vector.data[i])){
+        int x = vector.data[i];
+        vector.data[i] = vector.data[i+1];
+        vector.data[i+1] = x;
+    }
+
+    if(len == 1) return;
 }
 
-type popArray(struct SArray *a){
-	if(!isEmptyArray(a)){
-		type elem = a->arr[0];
-		memmove(a->arr, a->arr + 1, a->topIndex);	
-		return elem;
-	}
-
+int is_digit(char* str){
+    for (int i = 0; i < strlen(str) ; i++){
+        if (isdigit(str[i]) == 0) return 0;
+    }
+    return 1;
 }
 
-char top(struct S *a){
-        if(!isEmpty(a))
-            return a->firstElem->value;
-}
 
-type topArray(struct SArray *a){
-	if(!isEmptyArray(a))
-		return a -> arr[0];
-}
-
-int isEmpty(struct S *a){
-	return a->firstElem == NULL;
-}
-
-int isEmptyArray(struct SArray *a){
-	return a->topIndex == -1;
-}
-
-struct S* initS(){
-        struct S *a = malloc(sizeof(struct S));
-	a -> firstElem = NULL;
-        a -> headElem = NULL;
-        return a;
-}
-
-struct SArray* initSArray(){
-	struct SArray *a = malloc(sizeof(struct SArray));
-        a -> topIndex = -1;
-        return a;
-}
-
-int main(){
-	struct S *a1 = initS();
-	struct SArray *a2 = initSArray();
-        char value = 'k';
-	//printf("%ld\n", clock());	
-	//pushArray(a2, value);
-	//printf("%ld\n", clock());	
-	push(a1, value);
-	printf("%ld\n", clock());	
-
-	return 0;
+int main(int argc, char* argv[]) // на всякий случай
+{
+    
+    char ch;
+    if(argc!=2){
+        cout << "Применение: имя_программы <имя_файла>\n";
+        return 1;
+    }
+    
+    
+    
+  
+    return 0;
 }
